69307462d42b7c0d3fb00431391ef168a1909b56
Logan Merriam <loganmerriam@gmail.com>
2013-10-29 09:57:12 -0400

    Working (untested) scores function in entry

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -8,47 +8,16 @@ class EntriesController < ApplicationController
 
 		@entries = Entry.all
 
-		def makeScoreArray()
-			# Calculate the score for a given day
-			def makeScore(d)
-				score = 0;
-				# Sum of stools over the past week, weighting factor 2
-				score += Entry.where(created_at: (d - 7.day)..d).sum(:stools) * 2
-				# Avg of pain ratings over the past week, weighting factor 5
-				score += Entry.where(created_at: (d - 7.day)..d).average(:ab_pain) * 5
-				# Avg of general well-being ratings over the past week, weighting factor 7
-				score += Entry.where(created_at: (d - 7.day)..d).average(:general) * 7
-				# Add 20 for each complication present in latest entry
-				score += 20 if Entry.select(:complication_arthritis).where(created_at: d).last
-				score += 20 if Entry.select(:complication_erythema).where(created_at: d).last
-				score += 20 if Entry.select(:complication_fistula).where(created_at: d).last
-				score += 20 if Entry.select(:complication_other_fistula).where(created_at: d).last
-				score += 20 if Entry.select(:complication_iritis).where(created_at: d).last
-				# Add 20 if fever present in past week
-				score += 20 if Entry.where(created_at: (d - 7.day)..d, fever: true).exists?
-				# Add 30 if taking opiates in latest entry
-				score += 30 if Entry.select(:opiates).where(created_at: d).last
-				# Presence of abdominal mass in latest entry, weighting facto 10
-				score += Entry.select(:mass).where(created_at: d).last * 10
-				# Difference current_hematocrit from statistic norm
-				score += (46 - Entry.select(:hematocrit).where(created_at: d).last) * 6
-				# Percent deviation current_weight from normal weight
-				score += ((Entry.select(:weight_current).where(created_at: d).last - 140)/Entry.select(:weight_current).where(created_at: d).last) * 100
-				return score
-			end
-			# Push each day's score into an array
-			scoreArray = []
-			for entry in @entries
-				scoreArray.push(makeScore(entry.created_at))
-			end
-			return scoreArray
+		def make_scores()
+			@entries.map {|e| [e.date, e.score]}
 		end
+
 		# Init highcharts and pass it the array of scores
 		@chart = LazyHighCharts::HighChart.new('graph') do |f|
 			f.title({ :text=> 'Disease History' })
 			f.xAxis(:type => 'datetime')
 			f.legend(:enabled => false)
-			f.series(:type=> 'areaspline',:name=> 'CDAI Score',:data=> makeScoreArray())
+			f.series(:type=> 'areaspline',:name=> 'CDAI Score',:data=> make_scores())
 		end
 	end
 
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,66 @@
 class Entry < ActiveRecord::Base
    attr_accessible :stools, :ab_pain, :general, :complication_arthritis, :complication_iritis, :complication_erythema, :complication_fistula, :complication_other_fistula, :complication_fever, :opiates, :mass, :hematocrit, :weight_current
+
+
+	def score
+		score = 0;
+
+		now = Time.new
+		start_range = now.beginning_of_day - 7.days
+		end_range = now.end_of_day
+		past_entries = Entry.where("created_at >= ? and created_at <= ?", start_range, end_range).all
+
+		# past
+		score += stool_score(past_entries)
+		score += ab_pain_score(past_entries)
+		score += general_score(past_entries)
+
+		score += 20 if past_entries.select {|e| e.complication_fever}.length > 0
+
+		# current
+		[:complication_arthritis, :complication_erythema, :complication_fistula, 
+		 :complication_other_fistula, :complication_iritis].each do |attr|
+			score += 20 if self[attr]
+		end
+
+		score += 30 if opiates
+		score += mass * 10
+		score += (46 - hematocrit) * 6
+		score += weight_score
+
+		return score
+	end
+
+	def stool_score(past_entries)
+		past_entries.
+			map {|e| e.stools}.
+			inject(:+) * 2
+	end	
+
+	def ab_pain_score(past_entries)
+		past_entries.
+			map {|e| e.ab_pain}.
+			inject(:+) * 5
+	end	
+
+	def general_score(past_entries)
+		past_entries.
+			map {|e| e.general}.
+			inject(:+) * 7
+	end	
+
+	def weight_expected
+		140
+	end
+
+	def weight_score
+		factor = (weight_current - weight_expected) / weight_current * 100
+		[factor, -10].max
+	end
+
+
+	def date
+		created_at.to_datetime.to_i*1000
+	end
+
 end
