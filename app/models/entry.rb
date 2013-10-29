59a92f02b82441059eb22167167acfdea714cfa4
Logan Merriam <loganmerriam@gmail.com>
2013-10-29 10:29:28 -0400

    Unprotected created_at, seeded db

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,5 +1,5 @@
 class Entry < ActiveRecord::Base
-   attr_accessible :stools, :ab_pain, :general, :complication_arthritis, :complication_iritis, :complication_erythema, :complication_fistula, :complication_other_fistula, :complication_fever, :opiates, :mass, :hematocrit, :weight_current
+   attr_accessible :stools, :ab_pain, :general, :complication_arthritis, :complication_iritis, :complication_erythema, :complication_fistula, :complication_other_fistula, :complication_fever, :opiates, :mass, :hematocrit, :weight_current, :created_at
 
 
 	def score
