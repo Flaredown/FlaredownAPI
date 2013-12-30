30fdc2e90ba56a8cafabfa1db11c4ba10291ae13
Logan Merriam <loganmerriam@gmail.com>
2013-12-30 17:57:23 -0500

    Removed bad logged/unlogged code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
-  latest = Entry.last
-  now = Time.new
-  if latest.date > now.beginning_of_day.to_datetime.to_i*1000
-  	@today = "logged"
-  else
-  	@today = "unlogged"
-  end
-
 end
