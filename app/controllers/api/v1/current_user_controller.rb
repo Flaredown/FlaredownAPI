c02ab5d542f7653b064cddbe8c8796b4280c2da8
Graham Powrie <graham@asbp.io>
2014-11-06 14:44:58 -0200

    got rid of imported junk code

--- app/controllers/api/v1/current_user_controller.rb
+++ app/controllers/api/v1/current_user_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::CurrentUserController < Api::V1::BaseController
-  before_filter :only_current_user, except: [:index, :avatar, :favorite_pharmacy]
+  before_filter :only_current_user, except: [:index]
 
   # for current user lookup
   def index
@@ -9,45 +9,7 @@ class Api::V1::CurrentUserController < Api::V1::BaseController
   def show
     render json: current_user, serializer: CurrentUserSerializer
   end
-
-  def avatar
-    if params[:file]
-      current_user.update_attribute(:avatar, params[:file])
-      render json: {}, status: 200
-    else
-      render json: {}, status: 400
-    end
-  end
-
-  def favorite_pharmacy
-    favorite_pharmacy_params = params.require(:favorite_pharmacy).permit(
-      :dosespot_pharmacy_id, :store_name, :pharmacy_specialties, :address1, :address2,
-      :city, :state, :zip_code, :primary_phone, :primary_fax
-    )
-    pharmacy = Pharmacy.find_by(dosespot_pharmacy_id: favorite_pharmacy_params[:dosespot_pharmacy_id])
-    pharmacy ||= Pharmacy.create!(favorite_pharmacy_params)
-    current_user.pharmacy = pharmacy
-    if current_user.save
-      render json: CurrentUserSerializer.new(current_user).to_json, status: 200
-    else
-      render json: {:success => false, :errors => current_user.errors}, status: 400
-    end
-  end
-
-  def update
-    if current_user.update_attributes(user_params)
-      render json: CurrentUserSerializer.new(current_user).to_json, status: 200
-    else
-      render json: {:success => false, :errors => current_user.errors}, status: 400
-    end
-  end
-
   private
-  def user_params
-    params.require(:current_user).permit(
-      %i(prefix first_name middle_name last_name suffix phone_home phone_work phone_cell fax role dob gender email password password_confirmation).push(User.preference_definitions.keys.map{|k| "prefers_#{k}".to_sym}).flatten
-    )
-  end
   def only_current_user
     four_oh_four unless params[:id].to_i == current_user.id
   end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -10,20 +10,7 @@ class Api::V1::UsersController < Api::V1::BaseController
     render json: current_user, status: 200
   end
 
-  def update
-    if current_user.update_attributes(user_params)
-      render json: current_user, status: 200
-    else
-      render json: {:success => false, :errors => current_user.errors}, status: 400
-    end
-  end
-
   private
-  def user_params
-    params.require(:user).permit(
-      %i(prefix first_name middle_name last_name suffix phone_home phone_work phone_cell fax role email gender dob password password_confirmation).push(User.preference_definitions.keys.map{|k| "prefers_#{k}".to_sym}).flatten
-    )
-  end
   def only_current_user
     four_oh_four unless params[:id].to_i == current_user.id
   end
