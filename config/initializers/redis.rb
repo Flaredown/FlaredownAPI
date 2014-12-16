138899daebbc0d87fc88171dfbcbd285ab19176d
Graham Powrie <graham@asbp.io>
2014-12-16 13:02:36 -0200

    updated redis setup for heroku

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,9 @@
 db = Rails.env.test? ? 1 : 0
-REDIS = Redis.new(host: "127.0.0.1", port: ENV["REDIS_PORT"], db: db)
+
+if ENV["REDISCLOUD_URL"]
+  REDIS = Redis.new(:url => ENV["REDISCLOUD_URL"])
+else
+  REDIS = Redis.new(host: "127.0.0.1", port: ENV["REDIS_PORT"], db: db)
+end
+
 Resque.redis = REDIS
