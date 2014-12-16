a3bce222022642a0211b2c0b66d18e7651e42aec
umairabid-tkxel <umair.abid@tkxel.com>
2014-12-16 11:04:22 +0000

    [WIP] #47 - updated symptoms tests

--- spec/controllers/v1/symptoms_controller_spec.rb
+++ spec/controllers/v1/symptoms_controller_spec.rb
@@ -13,25 +13,30 @@ describe V1::SymptomsController, type: :controller do
     it "does not allow name duplicate names" do
       symptom = create :symptom, name: "duplicate"
       post :create, {name: 'duplicate'}
-      expect(json_response["name"][0]).to eq "Name already exists"
+      error_message = json_response["errors"]["fields"]["name"][0]["message"]
+      Rails.logger.debug error_message
+      expect(error_message).to eq "Name already exists"
       returns_code 400
     end
 
     it "does not allow name exceeding fifty charachters" do
       post :create, {name: "longer then ever symptom name that could ever be imagine on this very earth"}
-      expect(json_response["name"][0]).to eq "Name cannot be longer then 50 charachters"
+      error_message = json_response["errors"]["fields"]["name"][0]["message"]
+      expect(error_message).to eq "Name cannot be longer then 50 charachters"
       returns_code 400
     end
 
     it "only allows alphanumeric and spaces in name" do
       post :create, name: "hi()$%^&"
-      expect(json_response["name"][0]).to eq "Name can only include alphanumeric characters, hyphens and spaces"
+      error_message = json_response["errors"]["fields"]["name"][0]["message"]
+      expect(error_message).to eq "Name can only include alphanumeric characters, hyphens and spaces"
       returns_code 400
     end
 
     it "does not allow names with obscene words" do
       post :create, name: "fuck this shit"
-      expect(json_response["name"][0]).to eq "Please do not use obscene words"
+      error_message = json_response["errors"]["fields"]["name"][0]["message"]
+      expect(error_message).to eq "Please do not use obscene words"
       returns_code 400
     end
 
