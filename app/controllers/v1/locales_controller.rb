355b15596d911de9c6c2c2e1bc0358766012deb9
Graham Powrie <graham@asbp.io>
2014-12-30 22:08:04 -0200

    added draft hbi locale

--- app/controllers/v1/locales_controller.rb
+++ app/controllers/v1/locales_controller.rb
@@ -29,7 +29,7 @@ class V1::LocalesController < V1::BaseController
     locale_name = sanitize_filename(locale_params["locale"]).to_sym
     locale = yaml_to_json_style_interpolation!( File.open("#{Rails.root}/config/locales/#{locale_name}/#{locale_name}_base.yml").read )
 
-    if current_user.catalogs.present? and locale[locale_name] # Load each catalog on the current_user into the response
+    if current_user.catalogs.present? # Load each catalog on the current_user into the response
       locale[locale_name][:catalogs] ||= {}
 
       current_user.catalogs.each do |catalog|
