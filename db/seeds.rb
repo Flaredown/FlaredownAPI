e49bec784ec06df3c2cae3875fbffbb0e8c77569
Graham Powrie <graham@asbp.io>
2014-12-31 17:46:11 -0200

    cleaned up seeds a bit (don't need 500 entries really...)

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,11 +3,8 @@ REDIS.flushdb
 Entry.all.each{|e| e.destroy}
 User.all.each{|u| u.destroy}
 ActiveRecord::Base.connection.reset_pk_sequence!("users")
-# old_user=User.find_by_email("test@test.com")
-# old_user.delete if old_user
 
 u=User.create(id: 1, email: "test@test.com", password: "testing123", password_confirmation: "testing123", gender: "male", weight: 145, catalogs: ["hbi"])
-colin=User.create(id: 11, email: "colin@flaredown.com", password: "testing123", password_confirmation: "testing123", gender: "male", weight: 135, catalogs: ["symptoms"])
 
 # Add symptom names from :hbi_and_symptoms_entry
 ["droopy lips", "fat toes", "slippery tongue"].each do |name|
@@ -15,11 +12,13 @@ colin=User.create(id: 11, email: "colin@flaredown.com", password: "testing123", 
   u.active_symptoms << s.id
 end
 
-500.times do |n|
+# Add entries for test user
+200.times do |n|
   FactoryGirl.create :hbi_and_symptoms_entry, user: u, date: Date.today-n.days
 end
 
-# Add symptoms for user colin
+# Add Colin and his symptoms (entries sold separately)
+colin=User.create(id: 11, email: "colin@flaredown.com", password: "testing123", password_confirmation: "testing123", gender: "male", weight: 135, catalogs: ["symptoms"])
 ["pain", "fatigue", "digestive", "lightheadedness", "anxiety"].each do |name|
     s = colin.symptoms.create name: name, language: "en"
     colin.active_symptoms << s.id
