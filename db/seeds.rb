35c67aadd6775bcfe6794575759706094cc97902
Colin Gorrie <colin.gorrie@gmail.com>
2014-12-31 13:18:29 -0500

    Add five responses to symptoms catalog for user '11'.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@ ActiveRecord::Base.connection.reset_pk_sequence!("users")
 # old_user.delete if old_user
 
 u=User.create(id: 1, email: "test@test.com", password: "testing123", password_confirmation: "testing123", gender: "male", weight: 145, catalogs: ["hbi"])
+colin=User.create(id: 11, email: "colin@flaredown.com", password: "testing123", password_confirmation: "testing123", gender: "male", weight: 135, catalogs: ["symptoms"])
 
 # Add symptom names from :hbi_and_symptoms_entry
 ["droopy lips", "fat toes", "slippery tongue"].each do |name|
@@ -16,4 +17,10 @@ end
 
 500.times do |n|
   FactoryGirl.create :hbi_and_symptoms_entry, user: u, date: Date.today-n.days
+end
+
+# Add symptoms for user colin
+["pain", "fatigue", "digestive", "lightheadedness", "anxiety"].each do |name|
+    s = colin.symptoms.create name: name, language: "en"
+    colin.active_symptoms << s.id
 end
