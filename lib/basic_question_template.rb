68926fd771142250de405f3f86e266a793545af9
Graham Powrie <graham@asbp.io>
2015-03-12 21:14:32 -0300

    use helper function instead

--- lib/basic_question_template.rb
+++ lib/basic_question_template.rb
@@ -4,11 +4,11 @@ module BasicQuestionTemplate
       name: name,
       kind: :select,
       inputs: [
-        {value: 0, label: "basic_0", meta_label: "smiley"},
-        {value: 1, label: "basic_1", meta_label: nil},
-        {value: 2, label: "basic_2", meta_label: nil},
-        {value: 3, label: "basic_3", meta_label: nil},
-        {value: 4, label: "basic_4", meta_label: nil},
+        {value: 0, helper: "basic_0", meta_label: "smiley"},
+        {value: 1, helper: "basic_1", meta_label: nil},
+        {value: 2, helper: "basic_2", meta_label: nil},
+        {value: 3, helper: "basic_3", meta_label: nil},
+        {value: 4, helper: "basic_4", meta_label: nil},
       ]
     }]
   end
