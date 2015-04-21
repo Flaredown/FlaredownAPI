e80ffe026ae2ffcee733fc8ae5f0206fd054cafa
Graham Powrie <graham@asbp.io>
2015-04-21 12:23:09 -0300

    mandril settings for smtp

--- config/application.rb
+++ config/application.rb
@@ -41,5 +41,17 @@ module Flaredown
     # I18n.enforce_available_locales = true
 
 
+    config.action_mailer.smtp_settings = {
+      :address   => "smtp.mandrillapp.com",
+      :port      => 587,
+      :user_name => ENV["MANDRILL_USERNAME"],
+      :password  => ENV["MANDRILL_KEY"]
+    }
+
+    # ActionMailer Config
+    config.action_mailer.default_url_options = { :host => ENV["EMAIL_HOSTNAME"] }
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.raise_delivery_errors = false
   end
 end
