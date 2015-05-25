46aa04f304fb26584e93aa36ccd3c6c248662741
Graham Powrie <graham@asbp.io>
2015-05-25 20:20:04 -0300

    1.day for prod, not 10.seconds

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -136,7 +136,7 @@ class Entry < CouchRest::Model::Base
       # Keen.publish(:catalogs, catalog_to_keen)
     end
 
-    Resque.enqueue_at(10.seconds.from_now, EntrySendToKeen, {id: entry.id, timestamp: Time.now.to_s})
+    Resque.enqueue_at(1.day.from_now, EntrySendToKeen, {id: entry.id, timestamp: Time.now.to_s})
     # TODO reenable Pusher
     # entry.user.notify!("entry_processed", {entry_date: entry.date}) if entry.complete? and notify
     true
